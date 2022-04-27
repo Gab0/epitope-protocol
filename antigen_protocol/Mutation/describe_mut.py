a656b3f292510db3443a900d81b6fbf67aeedb81
Gabriel Araujo <gabriel_scf@hotmail.com>
2022-04-26 21:21:46 -0300

    Add new modules.

---
+++ antigen_protocol/Mutation/describe_mut.py
@@ -0,0 +1,89 @@
+
+from typing import Dict, List
+import os
+import csv
+from autogromacs.mdanalysis import process_simulation_name
+
+
+def is_specifier(fname: str,
+                 fractionary: bool,
+                 normal: bool) -> bool:
+    if fname.startswith("DUMMY"):
+        if fname.endswith(".pdb"):
+            return False
+
+        if "-" in fname:
+            return fractionary
+
+        return normal
+
+    return False
+
+
+def process_directory(
+        ROOT_DIR: str,
+        load_fractionary: bool,
+        load_normal: bool,
+        output_path: str) -> None:
+
+    mutation_specifiers = [
+        F
+        for F in os.listdir(ROOT_DIR)
+        if is_specifier(F, load_fractionary, load_normal)
+    ]
+
+    mutation_specifiers = sorted(mutation_specifiers)
+
+    specifiers = {}
+    for specifier in mutation_specifiers:
+        fpath = os.path.join(ROOT_DIR, specifier)
+        with open(fpath) as f:
+            W = []
+            muts = f.readlines()
+            for mut in muts:
+                W.append(mut.strip("\n"))
+        specifiers[specifier] = W
+
+    process_file_tex(specifiers, output_path + ".tex")
+    process_file_table(specifiers, output_path + ".csv")
+
+
+K = Dict[str, List[str]]
+
+
+def process_file_table(mutation_specifiers: K, output_path: str):
+    with open(output_path, 'w') as f:
+        fn = ['Identificador', 'Substituições']
+
+        out = csv.DictWriter(f, fn)
+        out.writeheader()
+        for mut, mutations in mutation_specifiers.items():
+            out.writerow({
+                fn[0]: process_simulation_name(mut),
+                fn[1]: "; ".join(mutations)
+            })
+
+
+def process_file_tex(mutation_specifiers: K, output_path: str):
+    IO = "\\begin{itemize}"
+    IE = "\\end{itemize}"
+
+    output = [IO]
+
+    for specifier_file, mutations in mutation_specifiers.items():
+        Name = process_simulation_name(specifier_file)
+
+        output.append("\\item " + Name.replace("#", "\\#"))
+        output.append(IO)
+        for mut in mutations:
+            output.append("\\item " + mut)
+
+        output.append(IE)
+    output.append(IE)
+
+    with open(output_path, 'w') as out:
+        out.write("\n".join(output))
+
+
+if __name__ == "__main__":
+    process_directory("/home/Science/MD", True, True, "muts")
---
+++ antigen_protocol/Mutation/structure_name.py
@@ -0,0 +1,54 @@
+import re
+
+
+def process_simulation_name(name: str) -> str:
+    """
+    Convert internal simulation codes into
+    readable names for labels etc...
+    """
+
+    # Parse single mutation name patterns.
+    mutation_pat = re.findall(r"mutation_(\w\d+\w)", name)
+
+    if mutation_pat:
+        mutation = mutation_pat[0]
+        return f"Mutação {mutation}"
+
+    # Parse variation name patterns.
+    number_pat = re.findall(r"\d+-{0,1}\d*", name)
+    identifier_pat = re.findall(r"^[^\d]+", name)
+
+    if number_pat:
+        number = number_pat[0]
+    else:
+        number = ""
+
+    domain_pattern = r"^(\w)_"
+    domains = re.findall(domain_pattern, name)
+
+    if domains:
+        name += f", domínio {domains[0]}"
+        name = re.sub(domain_pattern, "", name)
+
+    if identifier_pat:
+        identifier_map = {
+            "DUMMY": "Artificial",
+            "RANDOM": "Aleatória",
+            "NAT": "Natural",
+            "mutate": "Natural",
+            "mutation": "Natural",
+            "MUTATE": "Natural"
+        }
+        identifier_code = identifier_pat[0].split("_")[-1]
+        try:
+            identifier = identifier_map[identifier_code]
+        except KeyError:
+            identifier = "Desconhecido"
+
+    else:
+        return name
+
+    if number == "0":
+        return "Original"
+
+    return " ".join([identifier, number])
